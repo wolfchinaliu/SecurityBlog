30b1714be42ff3e40dc3e9bdf075608a1d2cc137
jerry <wolfchianliu@163.com>
2018-08-01 20:21:52 +0800

    修改JenkinsTest 丢失符号

--- src/main/java/com/liuzihu/blog/BlogApplication.java
+++ src/main/java/com/liuzihu/blog/BlogApplication.java
@@ -8,7 +8,7 @@ public class BlogApplication {
 
     public static void main(String[] args) {
         String s = null;
-        System.out.println(s.toString());
+        System.out.println(s.toString())
         SpringApplication.run(BlogApplication.class, args);
     }
 }
